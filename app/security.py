0550ce54000ef1b7a86e38069d3254c1ae4fb536
jgabriel1 <josegabriel@det.ufc.br>
2020-05-15 14:49:41 -0300

    User authentication functions setup.

--- app/security.py
+++ app/security.py
@@ -1,43 +1,66 @@
+from typing import Union
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from databases import Database
+from passlib.context import CryptContext
 from app.database import db
 from app.database.models import users_table, User
+from app.token import decode_token
 
 # All functions here grab user information from db while authenticating
 # their credentials and raising errors if they occur. All of this will
 # be abstracted by the last function which should just return the User
 # object that can be used in the routes.
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl='/token')
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/token')
 
 
-async def get_user_db(db, username: str):
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(password, hashed_password)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+async def get_user_db(db: Database, username: str) -> User:
     user = await db.fetch_one(
-        users_table.select(
-        ).where(
+        users_table.select().where(
             users_table.c.name == username
         )
     )
     return user
 
 
-async def decode_token(token: str) -> User:
-    """
-    This function should decode the token and
-    return the user from database.
-    """
-    user = await get_user_db(db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_schema)):
-    user = decode_token(token)
+async def authenticate_user(
+        db: Database, username: str, password: str) -> Union[bool, User]:
+    user = await get_user_db(db, username)
     if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid authentication credentials.",
-            headers={'WWW-Authenticate': 'Bearer'}
-        )
+        return False
+    if not verify_password(password, user.pwd_hash):
+        return False
     return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail='Invalid authentication credentials.',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+
+    payload = decode_token(token)
+    username: str = payload.get('sub')
+    if username is None:
+        raise credentials_exception
+
+    user = await get_user_db(db, username=username)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
 
 # https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
---
+++ app/token.py
@@ -0,0 +1,33 @@
+import jwt
+from fastapi import HTTPException
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+
+SECRET_KEY = 'supersecret'
+ALGORITHM = 'HS256'
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def create_access_token(
+        data: dict, expires_delta: timedelta = timedelta(days=7)) -> str:
+    expire: datetime = datetime.utcnow() + expires_delta
+
+    to_encode = data.copy()
+    to_encode.update({'exp': expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_token(token: str) -> dict:
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=401,
+            detail='Invalid authentication credentials.',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
