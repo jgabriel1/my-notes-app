90e21c80dec016467e76630cbad7257877731640
jgabriel1 <josegabriel@det.ufc.br>
2020-05-24 14:48:52 -0300

    LoginForm fetching data properly from api.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import login, notes
 from .deps import Database, get_db
@@ -17,6 +18,14 @@ async def shutdown():
     database: Database = await get_db()
     await database.disconnect()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(notes.router)
 app.include_router(login.router, prefix='/login')
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import api from '../services/api'
 
-const LoginForm = props => {
+import '../styles/LoginForm.css'
+
+const LoginForm = (props) => {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
 
-        const formData = new FormData(event.target) // not grabbing info
+        const data = new FormData(event.target)
+        const headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
 
-        console.log(formData)
-
-        api.post('login/token', { data: formData })
-            .then(response => console.log(response.body)) // problem with CORS
+        try {
+            const response = await api.post('login/token', data, { headers: headers })
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
 
     }
 
     return (
         <div className="loginFormContainer">
             <form onSubmit={handleSubmit}>
-                <label>Username:
-                <input type="text" name="username" id="username" required />
-                </label>
+                <div id="usernameContainer">
+                    <label htmlFor="usernameInput">Username:</label>
+                    <input type="text" name="username" id="usernameInput" required />
+                </div>
 
-                <label>Password:
-                <input type="password" name="password" id="password" required />
-                </label>
+                <div id="passwordContainer">
+                    <label htmlFor="passwordInput">Password:</label>
+                    <input type="password" name="password" id="passwordInput" required />
+                </div>
 
-                <button type="submit">Submit</button>
+                <button>Submit</button>
             </form>
         </div>
     )
